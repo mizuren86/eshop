7f453e69620125791b244456607789eafb7e5913
CrannyChen <e87e6x32@gmail.com>
2025-04-08 17:40:45 +0800

    頁面中文化 後台調整

---
+++ src/main/java/com/eeit1475th/eshop/cart/dto/OrderUpdateDTO.java
@@ -0,0 +1,16 @@
+package com.eeit1475th.eshop.cart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderUpdateDTO {
+	
+    private String shippingStatus;
+
+}
